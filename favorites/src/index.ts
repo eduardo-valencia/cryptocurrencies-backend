46c1883e2bd0a916cbceafa136a6d88791ec40d8
Eduardo Valencia <8025918@gmail.com>
2021-08-31 20:35:17 -0700

    Try replacing escaped chars in private key

--- favorites/src/index.ts
+++ favorites/src/index.ts
@@ -5,12 +5,17 @@ import adapter from './services/pool'
 import poolConfig from './config/pool'
 import keys from './config/keys'
 
+const getPrivateKeyWithReplacedEscapedKeys = (): string => {
+  const { firebasePrivateKey } = keys
+  return firebasePrivateKey.replace(/\\n/g, '\n')
+}
+
 const connectToFirebase = () => {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: keys.firebaseProjectId,
       clientEmail: keys.firebaseClientEmail,
-      privateKey: keys.firebasePrivateKey,
+      privateKey: getPrivateKeyWithReplacedEscapedKeys(),
     }),
     databaseURL: 'https://cryptocurrencies-2759e.firebaseio.com',
   })
