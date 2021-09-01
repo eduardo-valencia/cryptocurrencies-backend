4b67fd6412a7359397262956505f46abe5f78f42
Eduardo Valencia <8025918@gmail.com>
2021-08-31 20:39:51 -0700

    Allow any origin

--- favorites/src/app.ts
+++ favorites/src/app.ts
@@ -1,12 +1,11 @@
 import app from '@supercoder.dev/backend-helpers/dist/utils/tests/app'
 import cors from 'cors'
 
-import keys from './config/keys'
 import router from './routes'
 
 app.use(
   cors({
-    origin: keys.publicUrl,
+    origin: true,
     credentials: true,
   })
 )
