b05b5cb3d26937708c0ec340bd36bcdd2d033a75
Eduardo Valencia <8025918@gmail.com>
2023-12-29 03:49:56 +0000

    Explain why we added moduleNameMapper

--- cryptocurrencies/jest.config.ts
+++ cryptocurrencies/jest.config.ts
@@ -3,6 +3,9 @@ const config = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./src/test/setup.ts'],
   testPathIgnorePatterns: ['/dist/'],
+  /**
+   * Without this, importing Axios will throw an error during tests.
+   */
   moduleNameMapper: { '^axios$': require.resolve('axios') },
 }
 
